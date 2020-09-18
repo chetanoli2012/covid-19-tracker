a21704aa09aa4bf969b6d61d746eb2b1724af348
chetan <chetan.oli@squareyards.co.in>
2020-09-18 19:18:52 +0530

    Improve the sortData method

--- src/util.js
+++ src/util.js
@@ -1,12 +1,13 @@
-export const sortData = (data) =>{
+export const sortData = (data) => {
     const sortedData = [...data];
-    sortedData.sort((first,second)=>{
-        if(first.cases > second.cases){
-            return -1;
-        } else {
-            return 1
-        }
-    });
+    // sortedData.sort((first,second)=>{
+    //     if(first.cases > second.cases){
+    //         return -1;
+    //     } else {
+    //         return 1
+    //     }
+    // });
 
-    return sortedData;
+    // return sortedData;
+    return sortedData.sort((first, second) => (first.cases > second.cases ? -1 : 1))
 }
