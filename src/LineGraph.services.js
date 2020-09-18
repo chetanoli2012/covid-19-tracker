77c8557af7d125972ba5ec26cba8755aa7e66d7e
chetan <chetan.oli@squareyards.co.in>
2020-09-18 22:33:59 +0530

    Add the line graph services

---
+++ src/LineGraph.services.js
@@ -0,0 +1,19 @@
+import axiosInstance from "./axiosInstanc";
+
+/**
+ * Method to get countries data
+ */
+async function getHistoricalData() {
+    try {
+        let historicalDataUrl = `https://disease.sh/v3/covid-19/historical/all?lastdays=30`;
+        const result = await axiosInstance.get(`${historicalDataUrl}`);
+        return result;
+    } catch (err) {
+        console.error('error in getHistoricalData ', err);
+    }
+
+}
+
+export const lineGraphServices = {
+    getHistoricalData
+};
